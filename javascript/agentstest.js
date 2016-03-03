86dff1e5b0a4ec0c5004943623b8c24040d2ed61
gaurav gautam <gautam1168@gmail.com>
2016-03-03 21:50:14 +0530

    Adjusting height of canvas squares

--- javascript/agentstest.js
+++ javascript/agentstest.js
@@ -23,8 +23,8 @@ TrivialVacuumEnvironment.prototype.Render = function(elem){
                                                                     "rect"));
         this.squares[i].setAttribute("x", i*this.canvwidth/2);
         this.squares[i].setAttribute("y", 0);
-        this.squares[i].setAttribute("width", this.canvwidth/2-2);
-        this.squares[i].setAttribute("height", this.canvheight-1);
+        this.squares[i].setAttribute("width", this.canvwidth/2-8);
+        this.squares[i].setAttribute("height", this.canvheight-6);
         if (this.stat[i] == "Clean"){
             this.squares[i].setAttribute("fill", "white");
         }
