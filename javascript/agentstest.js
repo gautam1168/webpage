5cd3c51f71b9d79c7fc657f60164d009835b4059
gaurav gautam <gautam1168@gmail.com>
2016-03-03 22:19:14 +0530

    Color tweaks to button and border

--- javascript/agentstest.js
+++ javascript/agentstest.js
@@ -23,8 +23,8 @@ TrivialVacuumEnvironment.prototype.Render = function(elem){
                                                                     "rect"));
         this.squares[i].setAttribute("x", i*this.canvwidth/2);
         this.squares[i].setAttribute("y", 0);
-        this.squares[i].setAttribute("width", this.canvwidth/2-18);
-        this.squares[i].setAttribute("height", this.canvheight-17);
+        this.squares[i].setAttribute("width", this.canvwidth/2-19);
+        this.squares[i].setAttribute("height", this.canvheight-19);
         if (this.stat[i] == "Clean"){
             this.squares[i].setAttribute("fill", "white");
         }
